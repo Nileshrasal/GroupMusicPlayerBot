8ce00bb3e5d9e2b11e35858a271f6583736ef5d3
Nilesh Patil <90890819+Nileshrasal@users.noreply.github.com>
2021-09-17 21:49:54 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -72,4 +72,4 @@ async def skip(_, message: Message):
                 callsmusic.queues.get(message.chat.id)["file"]
             )
 
-        await message.reply_text("➡️ Skipped the current song!")
+        await message.reply_text("➡️ Skipped Are Prince Patil ke song ko skip mat karo yarr🥺!")
